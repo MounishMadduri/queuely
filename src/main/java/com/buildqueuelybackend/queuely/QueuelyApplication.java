7e3ceb400f5f5199f3cde7c4e969aa19d2341ba6
MounishMadduri <mounish5302@gmail.com>
2025-08-28 16:48:52 +0530

    Initial commit: Spring Boot project setup with folder structure

---
+++ src/main/java/com/buildqueuelybackend/queuely/QueuelyApplication.java
@@ -0,0 +1,13 @@
+package com.buildqueuelybackend.queuely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuelyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuelyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buildqueuelybackend/queuely/QueuelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildqueuelybackend.queuely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueuelyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
